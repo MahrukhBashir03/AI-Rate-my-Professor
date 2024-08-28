b9ee63c48772aab98222be5ea6bb6c89fc37da78
MahrukhBashir03 <163462081+MahrukhBashir03@users.noreply.github.com>
2024-08-28 23:24:46 +0500

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,53 @@
+'use client'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import { useState } from 'react'
+import Image from "next/image";
+
+export default function Home() {
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: `Hi! I'm the Rate My Professor support assistant. How can I help you today?`,
+    },
+  ])
+  const [message, setMessage] = useState('')
+  const sendMessage = async () => {
+    setMessage('')
+    setMessages((messages) => [
+      ...messages,
+      {role: 'user', content: message},
+      {role: 'assistant', content: ''},
+    ])
+  
+    const response = fetch('/api/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify([...messages, {role: 'user', content: message}]),
+    }).then(async (res) => {
+      const reader = res.body.getReader()
+      const decoder = new TextDecoder()
+      let result = ''
+  
+      return reader.read().then(function processText({done, value}) {
+        if (done) {
+          return result
+        }
+        const text = decoder.decode(value || new Uint8Array(), {stream: true})
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+          return [
+            ...otherMessages,
+            {...lastMessage, content: lastMessage.content + text},
+          ]
+        })
+        return reader.read().then(processText)
+      })
+    })
+  }
+  return (
+    
+  );
+}
